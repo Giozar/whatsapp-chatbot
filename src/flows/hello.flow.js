41d75f3754e61a3123e7a741d567691382f241d9
Giovanni Cortazar <manuelgiozar04@gmail.com>
2024-06-10 03:37:59 -0600

    Refactor hello flow and remove unused code

--- src/flows/hello.flow.js
+++
@@ -1,6 +0,0 @@
-const BotWhatsapp = require('@bot-whatsapp/bot')
-
-const helloFlow = BotWhatsapp.addKeyword(['Hola'])
-.addAnswer('Hola, soy un bot de ventas de Airontools, ¿En qué puedo ayudarte?')
-
-module.exports = helloFlow
--- src/flows/index.js
+++ src/flows/index.js
@@ -1,11 +1,9 @@
 const BotWhatsapp =  require('@bot-whatsapp/bot')
 
-const helloFlow = require('./hello.flow')
 const welcomeFlow = require('./welcome.flow');
 const cotizarFlow = require('./cotizar.flow');
 
 const flows =  BotWhatsapp.createFlow([
-    helloFlow,
     welcomeFlow,
     cotizarFlow,
 ])
