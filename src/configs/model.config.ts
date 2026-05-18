a38785ea94c928a0c9c28b91303861c1fd8e88a9
Manuel Giovanni Cortazar De La Cruz <manuelgiozar04@gmail.com>
2026-05-17 21:05:37 -0600

    docs: update AI model configuration comments for clarity

--- src/configs/model.config.ts
+++ src/configs/model.config.ts
@@ -1,6 +1,10 @@
 // ai/model.config.ts
 
 export const AI_MODEL_CONFIG = {
+  /*
+  Here you can configure the AI model you want to use.
+  - In this case, a model created with Modelfile in Ollama was used, based on llama3.2:b
+  */
   model: 'gio-chat:latest',
   usesModelfile: true,
 };
